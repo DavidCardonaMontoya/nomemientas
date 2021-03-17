1fdabc4d6b9f64f4c725c850ba9f82ee82ebc2c0
David <davidcardonam@gmail.com>
2021-03-16 23:44:11 -0500

    Se corrige la intención anémica de Juego
    Se logra adicionando métodos y comportamientos que por el momento no
    están implementados. También se agrega la presencia de dos mapas
    encargados de contener jugadores caracterizados por su id.
    
    El mapa jugadoresActivos contendrá a los jugadores con capital.
    
    El mapa jugadoresEliminados contendrá a los jugadores sin capital.
    
    Antes de que se inicie una ronda, se debe recorrer el mapa de
    jugadoresActivos buscando aquellos jugadores que durante la ronda recién
    terminada se quedaron sin capital, agregarlos a jugadoresEliminados y
    borrarlos de jugadoresActivos.

--- src/main/java/domain/juego/Juego.java
+++ src/main/java/domain/juego/Juego.java
@@ -2,23 +2,39 @@ package domain.juego;
 
 import co.com.sofka.domain.generic.AggregateEvent;
 import domain.juego.factory.JugadorFactory;
-import domain.juego.values.JuegoID;
-import domain.juego.values.JugadorID;
-import domain.juego.values.RondaID;
+import domain.juego.values.*;
 
 import java.util.Map;
 
 public class Juego extends AggregateEvent<JuegoID> {
 
     protected Boolean jugando;
-    protected Map<JugadorID, Jugador> jugadores;
+    protected Map<JugadorID, Jugador> jugadoresActivos;
+    protected Map<JugadorID, Jugador> jugadoresEliminados;
     protected RondaID rondaID;
+    protected Boolean juegoTerminado;
 
     public Juego(JuegoID entityId, JugadorFactory jugadorFactory) {
         super(entityId);
 //        appendChange(new JuegoCreado(entityId)).apply();
 //        jugadorFactory.jugadores()
 //                .forEach(jugador -> adicionarJugador(jugador.identity(), jugador.nombre(), jugador.capital()));
+    }
+
+    public Boolean jugando() {
+        return jugando;
+    }
+
+    public void agregarJugador(JugadorID jugadorID, Nombre nombre, Dinero capital){
+
+    }
+
+    public void iniciarJuego() {
+
+    }
+
+    public void iniciarRonda() {
+
     }
 
 
